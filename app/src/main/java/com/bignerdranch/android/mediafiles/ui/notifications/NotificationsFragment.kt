3027e00a828cfc0d22e89596d3899f292f7a7bfd
Yihui Shi <yihui.shi@outlook.com>
2021-02-06 17:37:38 -0800

    untested: add record ui

--- app/src/main/java/com/bignerdranch/android/mediafiles/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/bignerdranch/android/mediafiles/ui/notifications/NotificationsFragment.kt
@@ -1,22 +1,21 @@
 package com.bignerdranch.android.mediafiles.ui.notifications
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.paging.PagedList
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.mediafiles.MediaFilesApplication
 import com.bignerdranch.android.mediafiles.R
-import com.bignerdranch.android.mediafiles.discovery.model.MediaFile
 import com.bignerdranch.android.mediafiles.gas.model.FillGasEvent
 import com.bignerdranch.android.mediafiles.ui.recycleView.FillGasAdapter
-import com.bignerdranch.android.mediafiles.ui.recycleView.MediaFileAdapter
 
 class NotificationsFragment : Fragment() {
     private lateinit var notificationsViewModel: NotificationsViewModel
@@ -37,6 +36,12 @@ class NotificationsFragment : Fragment() {
 
         val textView = root.findViewById<TextView>(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, { s -> textView.text = s })
+
+        val button = root.findViewById<Button>(R.id.addGasRecordButton)
+        button.setOnClickListener {
+            Log.e("==YIHUI==", "setOnClickListener")
+        }
+
         return root
     }
 }
